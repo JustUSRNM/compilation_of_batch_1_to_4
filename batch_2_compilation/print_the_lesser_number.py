2e3279bb1cb671d567b01f9df72ec820ca58d5d3
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-13 17:37:01 +0800

    finished the print the lesser program

--- batch_2_compilation/print_the_lesser_number.py
+++ batch_2_compilation/print_the_lesser_number.py
@@ -1,2 +1,7 @@
 first_number = int(input("Please enter a number "))
 second_number = int(input("Please enter another number "))
+
+if first_number < second_number:
+    print (first_number)
+elif second_number < first_number:
+    print (second_number)
