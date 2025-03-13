635039ce2750fb90c6d0d14fa9fe707558eafea2
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-13 22:23:09 +0800

    finished the display the highest number program

--- batch_4_compilation/display_the_highest_number.py
+++ batch_4_compilation/display_the_highest_number.py
@@ -1,9 +1,9 @@
-smallest_number= None
+highest_number= None
 while True:
     try:
         inputted_number = int(input("Please enter a number "))
-        if  smallest_number is None or inputted_number < int(smallest_number) :
-            smallest_number = int(inputted_number)
+        if  highest_number is None or inputted_number > int(highest_number) :
+            highest_number = int(inputted_number)
     except ValueError:
-        print (smallest_number)
+        print (highest_number)
         break   
