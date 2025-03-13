78261d915e24c2a0f1747ebe61037ca6d588b5df
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-13 19:37:46 +0800

    finished the display all numbers with no duplicates program

--- batch_3_compilation/display_all_numbers_with_no_duplicates.py
+++ batch_3_compilation/display_all_numbers_with_no_duplicates.py
@@ -1,8 +1,9 @@
 inputted_number = [int(input("Enter a number ")) for i in range(10)]
-print (inputted_number)
 
-times_inpputed = {}
+times_inputted = {}
 for individual_number in inputted_number:
-    times_inpputed[individual_number] = times_inpputed.get(individual_number, 0) + 1
+    times_inputted[individual_number] = times_inputted.get(individual_number, 0) + 1
 
-print (times_inpputed)
+unique_numbers = [individual_number for individual_number in inputted_number if times_inputted[individual_number]==1]
+
+print (unique_numbers)
