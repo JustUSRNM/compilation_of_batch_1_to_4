808e79941948f9e372647b3cc42de3d9085834e9
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-13 22:19:30 +0800

    finished the display of the number with the most duplicates program

--- batch_4_compilation/display_the_number_with_the_most_duplicate.py
+++ batch_4_compilation/display_the_number_with_the_most_duplicate.py
@@ -1,5 +1,12 @@
+from collections import Counter
+
+number_list = []
 while True:
     try:
         inputted_number = int(input("Please enter a number "))
+        number_list.append(inputted_number)
     except ValueError:
+        duplicates_counter = Counter(number_list)
+        most_duplicates = max(duplicates_counter, key=duplicates_counter.get)
+        print (most_duplicates)
         break
