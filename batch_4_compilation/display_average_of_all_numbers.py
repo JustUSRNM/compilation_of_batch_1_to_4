c0ccb161bdf31bc5c66895c6ab01f78e808da43d
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-13 22:32:02 +0800

    finished the display everage of all numbers program; finished batch 4; finished compilation of batch 1 to 4

--- batch_4_compilation/display_average_of_all_numbers.py
+++ batch_4_compilation/display_average_of_all_numbers.py
@@ -1,5 +1,10 @@
+number_list=[]
+
 while True:
     try:
         inputted_number = int(input("Please enter a number "))
+        number_list.append(inputted_number)
     except ValueError:
+        average = sum(number_list)/len(number_list)
+        print (int(average))
         break
