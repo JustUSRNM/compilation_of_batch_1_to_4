c8ca8956e1bcf718d616cf95fd4cb6fddba57d0e
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-13 19:22:47 +0800

    added a system that counts how many times a number is inputted

--- batch_3_compilation/display_all_numbers_with_no_duplicates.py
+++ batch_3_compilation/display_all_numbers_with_no_duplicates.py
@@ -1,2 +1,8 @@
-inputted_number = [input("Enter a number ") for i in range(10)]
+inputted_number = [int(input("Enter a number ")) for i in range(10)]
 print (inputted_number)
+
+times_inpputed = {}
+for individual_number in inputted_number:
+    times_inpputed[individual_number] = times_inpputed.get(individual_number, 0) + 1
+
+print (times_inpputed)
