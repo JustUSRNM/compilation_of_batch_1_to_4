b9a2e05e8016a2c2134e881b91b45e94b2d78031
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-13 16:06:50 +0800

    added the input commands for the one's that needed it

---
+++ batch_1_compilation/print_the_bigger_number.py
@@ -0,0 +1,2 @@
+first_number = float(input("Please enter a number "))
+second_number = float(input("Please enter another number "))
---
+++ batch_1_compilation/print_the_quotient_with_2_decimals.py
@@ -0,0 +1,2 @@
+first_number = int(input("Please enter a number "))
+second_number = int(input("Please enter another number "))
