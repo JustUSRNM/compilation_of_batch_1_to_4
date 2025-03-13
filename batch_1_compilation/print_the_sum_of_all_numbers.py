042cebb56a559bcd6847f3db602f8a2a2ea94812
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-13 16:47:33 +0800

    finished the print the sum of 10 numbers program

--- batch_1_compilation/print_the_sum_of_all_numbers.py
+++ batch_1_compilation/print_the_sum_of_all_numbers.py
@@ -8,3 +8,6 @@ seventh_number = int(input("Please enter seventh number "))
 eighth_number = int(input("Please enter eighth number "))
 ninth_number = int(input("Please enter ninth number "))
 tenth_number = int(input("Please enter tenth number "))
+
+total_sum = first_number + second_number + third_number + fourth_number + fifth_number + sixth_number + seventh_number + eighth_number + ninth_number + tenth_number
+print (total_sum)
