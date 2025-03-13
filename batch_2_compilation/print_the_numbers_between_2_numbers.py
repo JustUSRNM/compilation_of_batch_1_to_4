03ea1ce044004a8f4a174068586c59bb16d2900f
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-13 18:13:36 +0800

    added a way to find the greater and lesser number from the two

--- batch_2_compilation/print_the_numbers_between_2_numbers.py
+++ batch_2_compilation/print_the_numbers_between_2_numbers.py
@@ -1,2 +1,9 @@
 first_number = int(input("Please enter a number "))
 second_number = int(input("Please enter another number "))
+
+if first_number > second_number:
+    greater_number = first_number
+    lesser_number = second_number
+elif second_number > first_number:
+    greater_number = second_number
+    lesser_number = first_number
