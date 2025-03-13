594a0d125000402c0558612bcf8027e67c215fe7
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-13 21:04:13 +0800

    finished the display unique when the number is unique and duplicate when not program

--- batch_3_compilation/display_unique_when_number_is_unique_and_duplicate_when_not.py
+++ batch_3_compilation/display_unique_when_number_is_unique_and_duplicate_when_not.py
@@ -1,5 +1,12 @@
+used_numbers = set()
+
 while True:
     try:
         inputted_number = int(input("Please enter a number "))
+        if inputted_number not in used_numbers:
+            print ('Unique')
+            used_numbers.add(inputted_number)
+        else:
+            print ('duplicate')
     except ValueError:
         break
